e31925161b551e4a96bf2aee719ff4595fb7afdf
Hele-S <silvahelenaldo@edu.unisinos.br>
2024-07-11 17:04:05 -0300

    acho que deu bom

--- Módulo 4/Aula 35/vite-project/src/components/TaskList.jsx
+++ Módulo 4/Aula 35/vite-project/src/components/TaskList.jsx
@@ -3,32 +3,27 @@ import style from './TaskList.module.css'
 function TaskList() {
     const [tasks, setTasks] = useState([]);
     const [inputValue, setInputValue] = useState('');
-    // const newItem = () => {
-    //     let item = document.createElement("button")
-    //     item.innerHTML = document.getElementById("prompt").value
-    //     return(item)}
+
     const addTask = () => {
         setTasks([...tasks, inputValue])
-        // document.getElementById("list").appendChild(newItem())
+        setInputValue('')
 
-        setInputValue('')}
-    
+    }
     return (
         <div>
             <h2>Lista de tarefas</h2>
             <div className={style.TESTE}>
-                <button  onClick={addTask}>Adcionar Tarefa</button>
-                <input id="prompt" type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+                <button onClick={addTask}>Adcionar Tarefa</button>
+                <input   type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
             </div>
-            <ul id="list">
-            {tasks.map((task, index) => (<>
+            <ul>
+                {tasks.map((task, index) => (<>
                     
-                    <li key={index} name={index}>
-                        {task}</li>
+                    <button className={style.delete}>Deletar</button>
+                    <li key={index}>{task}</li>
+                    <hr></hr>
                     </>))}
-                
             </ul>
-        
         </div>
     )
 }
