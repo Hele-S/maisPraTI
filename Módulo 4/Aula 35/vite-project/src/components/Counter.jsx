be954e63157ef403ecfd4cb5bc68613052eeb380
Hele-S <silvahelenaldo@edu.unisinos.br>
2024-07-11 18:13:54 -0300

    react pronto

--- Módulo 4/Aula 35/vite-project/src/components/Counter.jsx
+++ Módulo 4/Aula 35/vite-project/src/components/Counter.jsx
@@ -1,16 +1,17 @@
 import { useState } from "react"
-
+import style from "./Counter.module.css"
 function Counter() {
     const [count, setCount] = useState(0);
 
-    return (
-        <div>
-            <p>Você clicou {count} vezes</p>
-            <button onClick={() => setCount(count + 1)}>
+    return (<> <h2>Counter</h2>
+        <div className={style.content}>
+           
+            <button className={style.btn} onClick={() => setCount(count + 1)}>
                 Clique aqui
             </button>
+            <p>Você clicou {count} vezes</p>
         </div>
-    )
+        </>)
 }
 
 export default Counter
