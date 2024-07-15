b683d2a10b477d18ad5afa9020525e2c0650fb97
Hele-S <silvahelenaldo@edu.unisinos.br>
2024-07-15 20:35:24 -0300

    Contexto do tema funcionando

--- Módulo 4/Aula 39/Context/src/App.jsx
+++ Módulo 4/Aula 39/Context/src/App.jsx
@@ -6,12 +6,34 @@ const ThemeContext = createContext({ theme: 'light', toggle: () =>{} })
 
 function App() {
 
+  const [ theme, setTheme ] = useState('light')
+   
+  const toggleTheme = () => {
+    setTheme(currentTheme => currentTheme === 'light'? 'dark' : 'light')
+  }
+
   return (
     <>
       {/* <MyProvider>
         <MyChildren/>
       </MyProvider> */}
+      <ThemeContext.Provider value={{ theme, toggleTheme}}>
+        <div style={{backgroundColor: theme === 'light'? 'white':'black', color: theme === 'light'?'black':'white'}}>
+          <Toolbar />
+          <button onClick={toggleTheme}>Trocar o Tema</button>
+        </div>
+      </ThemeContext.Provider>
     </>
+  )
+}
+
+function Toolbar() {
+  const { theme } = useContext(ThemeContext)
+
+  return (
+    <div>
+      Tema utilizado - {theme}
+    </div>
   )
 }
 
