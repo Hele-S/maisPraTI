14f72f490ee3f307d567638c5e0fec5e63e1d3ca
Hele-S <silvahelenaldo@edu.unisinos.br>
2024-07-24 15:15:13 -0300

    Aula 39 pronta

--- Módulo 4/Aula 39/Desafio1/src/App.jsx
+++ Módulo 4/Aula 39/Desafio1/src/App.jsx
@@ -1,11 +1,37 @@
-import Desafio1 from './components/tentativa1'
+// import MyChildren from "./components/Filho"
+// import { MyProvider } from "./components/Contexto"
+import { useState, createContext, useContext } from 'react'
 
+const ThemeContext = createContext({ theme: 'light', toggleTheme: () =>{} })
 
 function App() {
 
-  return (<>
-    <Desafio1 />
-  </>
+  const [ theme, setTheme ] = useState('light')
+
+  const toggleTheme = () => {
+    setTheme(currentTheme => currentTheme === 'light' ? 'dark' : 'light')
+  }
+
+  return (
+    <>
+      <ThemeContext.Provider value={{ theme, toggleTheme }}>
+          <div>
+            <Toolbar />
+            <button onClick={toggleTheme}>Trocar o tema</button>
+          </div>
+      </ThemeContext.Provider>
+
+    </>
+  )
+}
+
+function Toolbar() {
+  const { theme } = useContext(ThemeContext)
+
+  return (
+    <div style={{background: theme === 'dark' ? 'grey' : 'blue', color: theme === 'dark' ? 'white' : 'white'}}>
+      Tema utilizado - {theme}
+    </div>
   )
 }
 
