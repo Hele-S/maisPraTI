044c99529c18bc31ada18dd289714e51ce4f7e82
Hele-S <silvahelenaldo@edu.unisinos.br>
2024-07-26 17:50:34 -0300

    Aula 41 Pronta

--- Módulo 4/Aula 41/HttpRequest/src/components/PutUser.jsx
+++ Módulo 4/Aula 41/HttpRequest/src/components/PutUser.jsx
@@ -1,10 +1,6 @@
 import axios from 'axios'
 
 function PutUser() {
-    // const newData = {
-    //     id : '2',
-    //     first_name: 'hele'
-    // }
 
     const upDate = () => {
         function test(response) {
@@ -12,11 +8,17 @@ function PutUser() {
             return response
         }
         axios.get(`https:reqres.in/api/users/3`).then(response => {
+
             alert(`Dado anterior: ${response.data.data.first_name}`)
+
             console.log(response.data.data)
+
             axios.put(`https:reqres.in/api/users/3`, test(response.data)).then(response => {
+
                 alert(`Dado novo: ${response.data.data.first_name}`)
+
                 console.log(response.data.data)
+
             }).catch(error => {
                 console.log("Erro no PUT :", error)
                 console.log(response.data)
