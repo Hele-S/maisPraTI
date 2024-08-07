410b76ad6c51d7f88a1f09c07ad565d419ff3268
Hele-S <silvahelenaldo@edu.unisinos.br>
2024-08-07 18:03:17 -0300

    Aula 42 pronta

--- Módulo 4/Aula 42/vite-project/src/App.jsx
+++ Módulo 4/Aula 42/vite-project/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import LanguageTranslator from './components/LanguageTranslator'
+import MovieSearch from './components/MovieSearch'
 
 function App() {
 
   return (
     <>
+   <MovieSearch/>
    <LanguageTranslator/>
     </>
   )
