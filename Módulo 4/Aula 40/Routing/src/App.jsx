bc70cd06d81979c01d0a79d95f2c95b2efb25b2e
Hele-S <silvahelenaldo@edu.unisinos.br>
2024-08-12 17:21:32 -0300

    Aula 40 revisão

--- Módulo 4/Aula 40/Routing/src/App.jsx
+++ Módulo 4/Aula 40/Routing/src/App.jsx
@@ -2,7 +2,7 @@ import  { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-
 import Navigation from './components/Navigation'
 import C1 from "./components/C1"
 import C2 from "./components/C2"
-// import C3 from "./components/C3"
+import C3 from "./components/C3"
 import Home from './components/Home'
 
 function App() {
@@ -19,7 +19,8 @@ function App() {
 
         <Route path='/' Component={C1}/>
         <Route path='/C2' Component={C2}/>
-        <Route path='/C3' element={<Navigate to='/'/>}/>
+        <Route path='/C4' Component={C3}/>
+        <Route path='/C3' element={<Navigate to ='/C4'/>}/>
       </Routes>
     </Router>
     </>
