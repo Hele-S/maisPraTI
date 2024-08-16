53def4daee1ac9141b657e4d4eebdf1dc30e6f5f
Hele-S <silvahelenaldo@edu.unisinos.br>
2024-08-16 01:22:13 -0300

    estilos de 'NavBar' em arquivo separado

--- Atividade 5/atv5_novo/multi-app/client/src/components/NavBarComponent.jsx
+++ Atividade 5/atv5_novo/multi-app/client/src/components/NavBarComponent.jsx
@@ -1,8 +1,8 @@
 //Importando a lib styled para estilização de componentes, e outros componentes e hooks necessários para atribuição de constantes, links e ícones
-import styled from "styled-components";
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom"
+import { /*Link,*/ useNavigate } from "react-router-dom"
 import  useVerifyJWT  from "../assets/hooks/useVerifyJWT" // Importando o Hook para verificar o JWT
+import { NavBar, NavBarToggle, StyledLink} from "../assets/styles/NavBar-styling"
 import {
     FaQrcode,
     FaSearch,
@@ -13,57 +13,7 @@ import {
     FaBars,
   } from "react-icons/fa";
 
-// Estiliza a barra de navegação.
-const NavBar = styled.div`
-  width: 240px;
-  background-color: #2c3e50;
-  color: white;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  padding: 20px;
-  box-shadow: 3px 0 15px rgba(0, 0, 0, 0.3);
 
-  @media (max-width: 768px) {
-    width: 100%;
-    height: auto;
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    display: ${({ open }) => (open ? "flex" : "none")};
-  }
-`;
-
-// Estiliza o botão de alternância da barra de navegação.
-const NavBarToggle = styled.div`
-  display: none;
-  position: absolute;
-  top: 20px;
-  right: 20px;
-  cursor: pointer;
-  z-index: 1000;
-
-  @media (max-width: 768px) {
-    display: block;
-  }
-`;
-
-// Estiliza os links na barra de navegação.
-const StyledLink = styled(Link)`
-  color: white;
-  text-decoration: none;
-  padding: 12px;
-  display: flex;
-  align-items: center;
-  gap: 10px;
-  transition: background-color 0.3s, color 0.3s;
-
-  &:hover {
-    background-color: #34495e;
-    color: #ecf0f1;
-  }
-`;
 
 /* eslint-disable react/prop-types */
 const NavBarComponent = ( {/*Redirect*/ logOut} ) => {
