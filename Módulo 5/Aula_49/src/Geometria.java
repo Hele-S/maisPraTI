4bad5067dc99a02a3c193a94e1dc11d9f9d7d80b
Hele-S <silvahelenaldo@edu.unisinos.br>
2024-09-05 08:51:11 -0300

    Aula 49 iniciada

---
+++ Módulo 5/Aula_49/src/Geometria.java
@@ -0,0 +1,9 @@
+public class Geometria {
+    public double calcularAreaRetangulo(double largura, double altura){
+        return largura * altura;
+    }
+
+    public static double multiplicar(double fator1, double fator2){
+      return fator1 * fator2;
+    }
+}
---
+++ Módulo 5/Aula_49/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        // Classe para calcular área do retângulo
+        Geometria geo = new Geometria();
+        double areaRetangulo = geo.calcularAreaRetangulo(5,10);
+        System.out.println("Área no retângulo: " + areaRetangulo);
+
+        double mult = Geometria.multiplicar(4,6);
+        System.out.println("Print da static 'multiplicar': " + mult);
+
+        // Classe para depositar valores e consultar
+        ContaBancaria conta = new ContaBancaria();
+        conta.depositar(1000);
+        System.out.println("Valor do saldo: " + conta.consultaSaldo());
+
+    }
+}
