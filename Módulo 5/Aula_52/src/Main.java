5fd889b6cf1902ce10c8803853851beaadc2a327
Hele-S <silvahelenaldo@edu.unisinos.br>
2024-09-18 10:37:22 -0300

    Aula 53 pronta

---
+++ Módulo 5/Aula_52/src/Main.java
@@ -0,0 +1,18 @@
+import reverseString.*;
+
+public class Main {
+    public static void main(String[] args) {
+//        StackExemple stack = new StackExemple();
+//
+//        stack.push(1);
+//        stack.push(2);
+//        stack.push(3);
+//        System.out.println("Elemento no topo: "  + stack.peek());
+//        System.out.println("Elemento removido: "  + stack.pop());
+//        System.out.println("Elemento no topo: "  + stack.peek());
+        ReverseString nome = new ReverseString();
+        nome.push("hele");
+        nome.peekAll();
+        nome.reverse();
+    }
+}
